28825ed05b643ab052c8e387303b285c52b407db
hippyy23 <hippyy2323@gmail.com>
2019-05-25 14:13:26 +0200

    changed channel

---
+++ bin/lib/cfg.py
@@ -0,0 +1,19 @@
+__author__ = "Munteanu Adrian"
+__copyright__ = "Copyright 2019, EloBot"
+__credits__ = ["Munteanu Adrian"]
+
+__version__ = "0.1"
+__email__ = "adrinarol@gmail.com"
+
+
+HOST = "irc.twitch.tv"
+PORT = 6667
+NICK = "s1mpleBOT"
+PASS = "oauth:e512tz3ey7weq8b3i733r6yauu7gl0"
+#oauth:3u13olvybu1gazhnz7dpp0fqc8tjwu arcadie bot
+#oauth:e512tz3ey7weq8b3i733r6yauu7gl0 s1mple bot
+CHANN = "s4tanax"
+#RATE = (20/30) # SPEED CHAT 
+
+oplist = {}
+elolist = []
