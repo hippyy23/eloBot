d66f97c68dec063d6931cb3d2043c83bad019f88
hippyy23 <hippyy2323@gmail.com>
2019-07-10 17:34:30 +0200

    changed interface

--- bin/lib/interface.py
+++ bin/lib/interface.py
@@ -1,8 +1,9 @@
 from tkinter import *
+import os
 #import lib.cfg
 
 box = Tk()
-box.geometry("370x220")
+box.geometry("350x170")
 box.resizable(False, False)
 
 faceitLogin = ""
@@ -11,8 +12,10 @@ status = BooleanVar()
 
 
 def quit():
-    box.destroy()
-
+    box.wm_state('withdraw')
+    login()
+    channel()
+    
 
 def login():
     lib.cfg.faceitLogin = entryLogin.get()
@@ -23,36 +26,34 @@ def channel():
 
 
 def onstartup():
-    pass
+    currentpath = os.popen('cd')
+    cmd = os.popen('')
 
 
 ###############################################################
 saveButton = Button(box, text="SAVE", command=quit)
-saveButton.place(x=180, y=200)
+saveButton.place(x=180, y=130)
+startButton = Button(box, text="START", command=quit)
+startButton.place(x=130, y=130)
 ###############################################################
-onstartupARadiobutton = Radiobutton(box, text="on startup ACTIVATE", command=onstartup, value=True, variable=status).place(x=50, y=100)
-onstartupFRadiobutton = Radiobutton(box, text="on startup DISACTIVATE", command=onstartup, value=False, variable=status).place(x=50, y=120)
+onstartupARadiobutton = Radiobutton(box, text="on startup ACTIVATE", command=onstartup, value=True, variable=status).place(x=10, y=30)
+onstartupFRadiobutton = Radiobutton(box, text="on startup DISACTIVATE", command=onstartup, value=False, variable=status).place(x=10, y=60)
 ###############################################################
-labelLogin = Label(box, text="INSERT YOUR FACEIT LOGIN")
+labelLogin = Label(box, text="FACEIT LOGIN")
 labelLogin.pack()
-labelLogin.place(x=135, y=5)
+labelLogin.place(x=180, y=5)
 ###############################################################
 entryLogin = Entry(box)
 entryLogin.pack()
-entryLogin.place(x=115, y=25)
+entryLogin.place(x=185, y=25)
 ###############################################################
-labelChannel = Label(box, text="INSERT YOUR CHANNEL NAME")
+labelChannel = Label(box, text="CHANNEL NAME")
 labelChannel.pack()
-labelChannel.place(x=125, y=55)
+labelChannel.place(x=180, y=55)
 ###############################################################
 entryChannel = Entry(box)
 entryChannel.pack()
-entryChannel.place(x=115, y=75)
-###############################################################
-insertButtonLogin = Button(box, text="insert", command=login)
-insertButtonLogin.place(x=310, y=25)
-###############################################################
-insertButtonChannel = Button(box, text="insert", command=channel)
-insertButtonChannel.place(x=310, y=75)
+entryChannel.place(x=185, y=75)
+
 
 box.mainloop()
