fc434627f7aa7f3beb8423736a190b63b01273a6
hippyy23 <hippyy2323@gmail.com>
2019-11-20 19:32:48 +0100

    removed gatherdata and tkinter interface

--- bin/lib/gatherdata.py
+++
@@ -1,8 +0,0 @@
-import lib.cfg
-
-lib.cfg.faceitLogin = str(input("Insert your faceit login: "))
-lib.cfg.CHANN = str(input("Insert your channel name: "))
-
-if (str(input("Start bot with windows\n[y/n]: ")) == "y"):
-    lib.cfg.onstartup = True
-
